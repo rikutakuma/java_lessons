1b57bc095ab01312a208132d3c70562fedc9e182
榎本拓真 <t3460515@gmail.com>
2021-02-19 22:50:28 +0900

    java

--- MyApp.java
+++ MyApp.java
@@ -28,38 +28,49 @@ public class MyApp {
 		// int i = (int)d;
 		// System.out.println(i);
 
-		int i = 10;
-		double d = (double)i / 4;
-		System.out.println(d);
-
-		int score = 45;
-		// if (score > 80) {
-		// 	System.out.println("Great!");			
-		// } else if (score > 60) {
-		// 	System.out.println("Good!");
-		// } else {
-		// 	System.out.println("so so ...!");
+		int i = 0;
+		// while (i < 10) {
+		// 	System.out.println(i);
+		// 	i++;
 		// }
-		String msg = score > 80 ? "Great!" : "so so ... !";
-    	System.out.println(msg);
 
-    	String signal = "green";
-    	switch (signal) {
-    		case "red":
-    			System.out.println("stop!");
-    			break;
-    		case "blue":
-    		case "green":
-    			System.out.println("go!");
-    			break;
-    		case "yellow":
-    			System.out.println("caution!");
-    			break;
-    		default:
-    			System.out.println("wrong signal!");
-    			break;
-    		}
-	}
+		do {
+			System.out.println(i);
+			i++;
+			} while (i < 10);
+		}
 
+		// int i = 10;
+		// double d = (double)i / 4;
+		// System.out.println(d);
 
+	// 	int score = 45;
+	// 	// if (score > 80) {
+	// 	// 	System.out.println("Great!");			
+	// 	// } else if (score > 60) {
+	// 	// 	System.out.println("Good!");
+	// 	// } else {
+	// 	// 	System.out.println("so so ...!");
+	// 	// }
+	// 	String msg = score > 80 ? "Great!" : "so so ... !";
+ //    	System.out.println(msg);
+
+ //    	String signal = "green";
+ //    	switch (signal) {
+ //    		case "red":
+ //    			System.out.println("stop!");
+ //    			break;
+ //    		case "blue":
+ //    		case "green":
+ //    			System.out.println("go!");
+ //    			break;
+ //    		case "yellow":
+ //    			System.out.println("caution!");
+ //    			break;
+ //    		default:
+ //    			System.out.println("wrong signal!");
+ //    			break;
+ //    		}
+	// }
 }
+
